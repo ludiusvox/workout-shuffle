b22c0e4c60a2ec83e7be43fe9ded316a54546e67
Aaron Linder <ludiusvox@gmail.com>
2026-04-14 19:48:03 -0400

    just added final files

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.workout.shuffle',
+  appName: 'workout-shuffle',
+  webDir: 'dist'
+};
+
+export default config;
